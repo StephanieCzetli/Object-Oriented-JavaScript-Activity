0987feb6cee6bb714ec67a044ba2182a21ca1702
Stephanie Czetli <stephanieczetli@gmail.com>
2021-01-25 15:49:51 -0500

    2nd commit - revised part 1

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
 
-//create an object and add the following properties: likes, comments, shares, name, photos
-
 let post = {
-    name: "Kaleb Burd",
-    message: "@coderhino why is your code hot garbage?",
-    comments: "Code Differently is awesome!",
-    likes: 5,
-    location: "Wilmington, DE",
-//add a few functions: tag, share
-    tag: function () {
+    name: "Stephanie Czetli",
+    message: "Hey Everyone!",
+    comments: "I'm learning a lot at Code Differently!",
+    likes: 3,
+    // function to add a "Like" increases existing number (3) by 1
+    addaLike: function (num = 1) { 
+        this.likes += num;
+    },
+    location: "Middletown",
+    tag: function(){
         console.log('this function is called tag');
     },
-    share: function () {
+    share: function(){
         console.log('this function is called share');
     }
 };
 
-
-//Use dot notation to access & update the value of a property -> objectName.property = newPropertyValue;
-post.name = "Team Bitcoin";
-post.message = "Learning to code is hard work and it will pay off!";
-post.likes = 25;
-post.location = "Saudi Arabia";
-
-//add a new property called "shares" to the object
-post.shares = 10;
-
-console.log(post.tag());
-
-
-
-//User bracket notation to access & update the value of a property - objectName ['property'] = newPropertyValue;
-
-post['name'] = "Winston Jefferson";
-post['likes'] = 10;
 console.log(post);
+
+//ACCESS w/ dot notation: console.log(objectName.propertyName);
+console.log(post.name);
+console.log(post.message);
+console.log(post.comments);
+
+//access & UPDATE w/ dot notation: objectName.propertyName = "newValue";
+post.name = "Paige Czetli";
+post.message = "I'm a very creative artist!";
+post.likes = "20";
+
+//ACCESS w/ bracket notation: console.log(objectName['propertyName']);
+console.log(post['name']);
+console.log(post['message']);
+console.log(post['likes']);
+
+//access & UPDATE w bracket notation: objectName['property'] = newValue;
+post['name'] = "Rob Czetli";
+post['message'] = "I'm an audiophile!";
+post['likes'] = "10";
+console.log(post['name']);
+
 
 post['tag']();
 post.tag();
@@ -42,5 +47,8 @@ post.tag();
 post['share']();
 post.share();
 
-//delete a property from an object
-delete post.likes;
+
+//add a new property called "shares" to the object
+post.shares = 10;
+
+console.log(post.tag());
